55e65372bf59568deebc250c959cb71f690e045c
Dominic Reichl <office@dominicreichl.com>
2024-12-11 18:42:26 +0100

    update config

--- src/config.py
+++ src/config.py
@@ -6,22 +6,28 @@ from pathlib import Path
 
 @dataclass
 class Config:
-    def __init__(self):
-        self.src_path = str(Path(__file__).parent)
-        self.feature_set = "medium"
-        self.experiment_name = "initial"
-        self.numerai_data_version = "v5.0"
-        self.data_asset_name = "numerai"
-        self.data_asset_version = dt.datetime.now().strftime("%Y-%m")
-        self.data_asset_uri = (
-            f"azureml:{self.data_asset_name}:{self.data_asset_version}"
-        )
-        with open("resources.json") as file:
-            resources = json.load(file)
-        self.subscription_id = resources["subscription_id"]
-        self.resource_group = resources["resource_group"]
-        self.workspace_name = resources["workspace_name"]
-        self.compute_instance = resources["compute_instance"]
-        self.environment_name = resources["environment_name"]
-        self.conda_file_path = str(Path(__file__).parent.parent / "conda.yml")
-        self.latest_env_name = None
+    # PARAMS
+    experiment_name = "initial"
+    feature_set = "medium"
+
+    # DATA
+    main_target = "target_cyrusd_20"
+    numerai_data_version = "v5.0"
+    data_asset_name = "numerai"
+    data_asset_version = dt.datetime.now().strftime("%Y-%m")
+    data_asset_uri = f"azureml:{data_asset_name}:{data_asset_version}"
+
+    # AZURE
+    with open("resources.json") as file:
+        resources = json.load(file)
+    subscription_id = resources["subscription_id"]
+    resource_group = resources["resource_group"]
+    workspace_name = resources["workspace_name"]
+    compute_instance = resources["compute_instance"]
+    environment_name = resources["environment_name"]
+
+    latest_env_name = ""
+
+    # PATHS
+    src_path = str(Path(__file__).parent)
+    conda_file_path = str(Path(__file__).parent.parent / "conda.yml")
