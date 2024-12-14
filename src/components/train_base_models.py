b67e70fd47561e26bceed13ad1ff29b015298db7
Dominic Reichl <office@dominicreichl.com>
2024-12-14 11:23:44 +0100

    fix(components): move mlflow run context further above to track model training time properly

--- src/components/train_base_models.py
+++ src/components/train_base_models.py
@@ -25,17 +25,19 @@ with mlflow.start_run():
     for col in target_cols:
         target = col.split("_")[1]
         model_name = f"base_{target}"
-        model = Model(name=model_name, main_target=args["main_target"], hparams=hparams)
-        model.fit(x_train, y_train, col)
-        predictions = model.predict(x_val)
-        metrics = model.evaluate(predictions, y_val)
-        with mlflow.start_run(run_name=target, nested=True):
+        with mlflow.start_run(run_name=model_name, nested=True):
+            model = Model(
+                name=model_name, main_target=args["main_target"], hparams=hparams
+            )
+            model.fit(x_train, y_train, col)
+            model.save(models_dir)
+            predictions = model.predict(x_val)
+            metrics = model.evaluate(predictions, y_val)
             mlflow.set_tag("target", col)
             mlflow.log_metrics(metrics)
             mlflow.lightgbm.log_model(model, model_name, input_example=x_val.iloc[:2])
-            model.save(models_dir)
-        y_val[f"pred_{target}"] = predictions
-        corrs[model_name] = metrics["corr_mean"]
+            y_val[f"pred_{target}"] = predictions
+            corrs[model_name] = metrics["corr_mean"]
 
 y_val.to_csv(os.path.join(models_dir, "predictions.csv"), index=False)
 with open(os.path.join(models_dir, "model_corrs.json"), "w") as f:
